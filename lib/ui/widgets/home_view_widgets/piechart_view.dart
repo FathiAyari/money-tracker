b6fbaf3ae3e1b586b6a3843b079a3dfc68e86069
FathiAyari <fathi.ayari@kelibia.r-iset.tn>
2023-03-08 02:23:29 +0100

    chart view chip choice

--- lib/ui/widgets/home_view_widgets/piechart_view.dart
+++ lib/ui/widgets/home_view_widgets/piechart_view.dart
@@ -1,6 +1,7 @@
 import 'package:chips_choice/chips_choice.dart';
 import 'package:flutter/material.dart';
 import 'package:moneymanager/core/enums/viewstate.dart';
+import 'package:moneymanager/ui/shared/dimensions/dimensions.dart';
 import 'package:moneymanager/ui/views/base_view.dart';
 import 'package:pie_chart/pie_chart.dart';
 
@@ -25,30 +26,63 @@ class PieChartView extends StatelessWidget {
                   padding: const EdgeInsets.all(8.0),
                   child: Card(
                     elevation: 4,
-                    child: Column(
-                      children: <Widget>[
-                        ChipsChoice<int>.single(
-                          value: model.selectedMonthIndex,
-                          wrapped: true,
-                          choiceItems: C2Choice.listFrom<int, String>(
-                            source: model.months,
-                            value: (i, v) => i,
-                            label: (i, v) => v,
+                    child: Container(
+                      width: Constants.screenWidth,
+                      child: Column(
+                        children: <Widget>[
+                          ChipsChoice<int>.single(
+                            choiceStyle: C2ChipStyle.filled(
+                                backgroundOpacity: 0,
+                                pressedStyle: const C2ChipStyle(
+                                  backgroundColor: Colors.white,
+                                  shadowColor: Colors.white,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(25),
+                                  ),
+                                ),
+                                selectedStyle: const C2ChipStyle(
+                                  backgroundColor: Colors.white,
+                                  borderColor: Colors.red,
+                                  borderWidth: 2,
+                                  borderStyle: BorderStyle.solid,
+                                  borderOpacity: 0.9,
+                                  checkmarkColor: Colors.red,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(25),
+                                  ),
+                                ),
+                                checkmarkStyle: C2ChipCheckmarkStyle.round,
+                                avatarForegroundColor: Colors.red,
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(25),
+                                ),
+                                avatarForegroundStyle: TextStyle(color: Colors.red),
+                                avatarBackgroundColor: Colors.red,
+                                checkmarkColor: Colors.red),
+                            choiceCheckmark: true,
+                            value: model.selectedMonthIndex,
+                            wrapped: true,
+                            choiceItems: C2Choice.listFrom<int, String>(
+                              source: model.months,
+                              value: (i, v) => i,
+                              label: (i, v) => v,
+                            ),
+                            onChanged: (val) => model.changeSelectedMonth(val),
                           ),
-                          onChanged: (val) => model.changeSelectedMonth(val),
-                        ),
-                        ChipsChoice<int>.single(
-                          value: model.type == 'income' ? 0 : 1,
-                          wrapped: true,
-                          choiceItems: C2Choice.listFrom<int, String>(
-                            source: model.types,
-                            value: (i, v) => i,
-                            label: (i, v) => v,
+                          ChipsChoice<int>.single(
+                            value: model.type == 'income' ? 0 : 1,
+                            wrapped: true,
+                            choiceCheckmark: true,
+                            choiceItems: C2Choice.listFrom<int, String>(
+                              source: model.types,
+                              value: (i, v) => i,
+                              label: (i, v) => v,
+                            ),
+                            onChanged: (val) => model.changeType(val),
                           ),
-                          onChanged: (val) => model.changeType(val),
-                        ),
-                        model.dataMap.length == 0 ? Text('No Data for this month') : PieChart(dataMap: model.dataMap),
-                      ],
+                          model.dataMap.length == 0 ? Text('No Data for this month') : PieChart(dataMap: model.dataMap),
+                        ],
+                      ),
                     ),
                   ),
                 ),
